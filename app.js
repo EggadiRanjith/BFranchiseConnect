00fa8e70e25d95d27e1cb5f37002c4713befb4a9
EggadiRanjith <116551342+EggadiRanjith@users.noreply.github.com>
2024-03-26 08:21:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -329,3 +329,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Internal Server Error' });
 });
 
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
